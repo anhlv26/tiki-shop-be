7de4a531000f2b65c1560ab8168051a328c93e4a
vietanh <vietanhle2662001@gmail.com>
2024-08-29 14:59:17 +0700

    feature login user and jsonwebtoken

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -30,4 +30,34 @@ const createUser = async (req, res) => {
   }
 };
 
-module.exports = { createUser };
+const loginUser = async (req, res) => {
+  const { email, password, confirmPassword, phone, name } = req.body;
+  try {
+    const reg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isCheckEmail = reg.test(email);
+    if (!email || !password || !confirmPassword || !phone || !name) {
+      return res.status(200).json({
+        status: "err",
+        message: "The input is required",
+      });
+    } else if (!isCheckEmail) {
+      return res.status(200).json({
+        status: "err",
+        message: "The email is invalid",
+      });
+    } else if (password !== confirmPassword) {
+      return res.status(200).json({
+        status: "err",
+        message: "The password and confirm password do not match",
+      });
+    }
+
+    console.log(req.body);
+    const response = await UserService.loginUser(req.body);
+    return res.status(200).json(response);
+  } catch (e) {
+    res.status(404).json({ message: e });
+  }
+};
+
+module.exports = { createUser, loginUser };
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,22 +1,26 @@
 const User = require("../models/UserModel");
+const bcrypt = require("bcrypt");
+const { generalAccessToken, generalRefreshToken } = require("./JwtService");
 
 const createUser = (newUser) => {
   return new Promise(async (resolve, reject) => {
-    const { email, password, confirmPassword, phone, name } = newUser;
+    const { email, password, phone, name } = newUser;
     try {
       const checkUser = await User.findOne({
         email: email,
       });
-      if(checkUser){
+      if (checkUser) {
         resolve({
-          status:"ok",
+          status: "ok",
           message: "user already exists",
         });
       }
+
+      const hash = bcrypt.hashSync(password, 10);
+
       const createUser = await User.create({
         email,
-        password,
-        confirmPassword,
+        password: hash,
         phone,
         name,
       });
@@ -33,4 +37,48 @@ const createUser = (newUser) => {
   });
 };
 
-module.exports = { createUser };
+const loginUser = (userLogin) => {
+  return new Promise(async (resolve, reject) => {
+    const { email, password, phone, name } = userLogin;
+    try {
+      const checkUser = await User.findOne({
+        email: email,
+      });
+      if (checkUser === null) {
+        resolve({
+          status: "ok",
+          message: "user doesn't exist",
+        });
+      }
+      const comparePassword = bcrypt.compareSync(password, checkUser.password);
+      console.log(comparePassword);
+      if (!comparePassword) {
+        resolve({
+          status: "ok",
+          message: "the password is incorrect",
+        });
+      }
+
+      const access_token = await generalAccessToken({
+        id: checkUser._id,
+        isAdmin: checkUser.isAdmin,
+      });
+
+      const refresh_token = await generalRefreshToken({
+        id: checkUser._id,
+        isAdmin: checkUser.isAdmin,
+      });
+
+      resolve({
+        status: "ok",
+        message: "success",
+        access_token,
+        refresh_token,
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+module.exports = { createUser, loginUser };
