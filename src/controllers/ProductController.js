41fc0598185425d87fe4c3877ee5dab603b7104c
vietanh <vietanhle2662001@gmail.com>
2024-09-05 23:12:52 +0700

    feature sort and filter

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -52,10 +52,12 @@ const deleteProduct = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
   try {
-    const { limit, page } = req.query;
+    const { limit, page, sort, filter } = req.query;
     const response = await ProductService.getAllProducts(
-      Number(limit),
-      Number(page)
+      Number(limit) || 8,
+      Number(page) || 0,
+      sort,
+      filter
     );
     return res.status(200).json(response);
   } catch (e) {
--- src/services/JwtService.js
+++ src/services/JwtService.js
@@ -1,6 +1,5 @@
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
-
 dotenv.config();
 
 const generalAccessToken = async (payload) => {
